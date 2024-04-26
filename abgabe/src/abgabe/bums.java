db71efc262038a44f35614bace3f8ae05f2a9e5d
Janne761 <janu10.jn@gmail.com>
2024-04-26 13:37:25 +0200

    a

---
+++ abgabe/src/abgabe/bums.java
@@ -0,0 +1,14 @@
+package abgabe;
+
+import classen.soldat;
+
+public class bums {
+
+	public static void main(String[] args) {
+		
+		soldat soldat1 = new soldat(2,2);
+		
+		System.out.println(soldat1.getA());
+	}
+
+}
---
+++ abgabe/src/classen/soldat.java
@@ -0,0 +1,25 @@
+package classen;
+
+public class soldat {
+int a;
+
+int b;
+
+public soldat(int a, int b) {
+	
+	this.a = a;
+	this.b = b;
+}
+
+public int getA() {
+	return a;
+}
+
+public int getB() {
+	return b;
+}
+
+
+
+
+}
