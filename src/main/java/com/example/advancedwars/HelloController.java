32e62b88410d5e19fd13f46a696a437b288dae9c
Philipp Staudinger <philipp.eckhard.staudinger@gmail.com>
2024-05-07 19:31:11 +0200

    .

--- src/main/java/com/example/advancedwars/HelloController.java
+++ src/main/java/com/example/advancedwars/HelloController.java
@@ -28,31 +28,39 @@ public class HelloController implements Initializable {
     }
 
     private void loadMap() {
-        // Iteriere über das 2D-Array und erstelle Rechtecke mit entsprechender Farbe basierend auf den Werten
+        // Iteriere über das 2D-Array und erstelle Bildansichten basierend auf den Werten
         for (int y = 0; y < this.model.map.length; y++) {
             for (int x = 0; x < this.model.map[y].length; x++) {
-                Rectangle rectangle = new Rectangle(50, 50);
+                ImageView imageView = new ImageView();
                 switch (this.model.map[y][x]) {
                     case 0:
-                        rectangle.setFill(Color.GREEN);
+                        imageView.setImage(new Image(getClass().getResourceAsStream("/images/gras.png")));
                         break;
                     case 1:
-                        rectangle.setFill(Color.SADDLEBROWN);
+                        imageView.setImage(new Image(getClass().getResourceAsStream("/images/wood.png")));
                         break;
                     case 2:
-                        rectangle.setFill(Color.GREY);
+                        imageView.setImage(new Image(getClass().getResourceAsStream("/images/mountain.png")));
                         break;
                     case 3:
-                        rectangle.setFill(Color.BLUE);
+                        imageView.setImage(new Image(getClass().getResourceAsStream("/images/sea.png")));
+                        break;
+                    // Füge weitere Fälle hinzu, wenn du mehr Bilder für andere Werte benötigst
                 }
-                mapGridPane.add(rectangle, x, y);
+
+                imageView.setFitWidth(50);
+                imageView.setFitHeight(50);
+
+                mapGridPane.add(imageView, x, y);
+
+                // Überprüfe, ob eine Truppe auf diesem Feld steht
                 if (this.model.troops[y][x] != null) {
                     String troopImgPath = this.model.troops[y][x].getTroopImg();
                     Image troopImg = new Image(getClass().getResourceAsStream(troopImgPath));
-                    ImageView imageView = new ImageView(troopImg);
-                    imageView.setFitWidth(35);
-                    imageView.setFitHeight(35);
-                    mapGridPane.add(imageView, x, y);
+                    ImageView troopImageView = new ImageView(troopImg);
+                    troopImageView.setFitWidth(35);
+                    troopImageView.setFitHeight(35);
+                    mapGridPane.add(troopImageView, x, y);
                 }
             }
         }
