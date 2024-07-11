f6115b3dbc95a1a27797470262e5a55753a4e35a
Janne761 <janu10.jn@gmail.com>
2024-07-11 14:29:05 +0200

    hm

--- src/main/java/com/example/advancedwars/Artillery.java
+++ src/main/java/com/example/advancedwars/Artillery.java
@@ -8,10 +8,11 @@ public class Artillery extends Troop{
     }
     public ArrayList<int[]> getAttackRange(int xlength, int ylength){
         ArrayList<int[]> range = new ArrayList<int[]>();
+        //unten rechts
         int Y=-3;
         int x=0;
-        //unten rechts
-        /*for(int i = 3; i >0 ; i--){
+
+        for(int i = 3; i >0 ; i--){
             if(this.xpos+x<xlength&&this.ypos-Y<ylength&&this.xpos+x>=0&&this.ypos-Y>=0){range.add(new int[]{this.xpos+x, this.ypos - Y});}
 
             Y+=1;
@@ -22,14 +23,12 @@ public class Artillery extends Troop{
          Y=3;
          x=0;
         for(int i = 0; i <3 ; i++){
-            if(this.xpos<xlength&&this.ypos-Y<ylength&&this.xpos+x>=0&&this.ypos-Y>=0){range.add(new int[]{this.xpos+x, this.ypos - Y});
-                System.out.println((this.xpos+x)+" "+(this.ypos-Y));
+            if(this.xpos+x<xlength&&this.ypos-Y<ylength&&this.xpos+x>=0&&this.ypos-Y>=0){range.add(new int[]{this.xpos+x, this.ypos - Y});
             }
-
             Y-=1;
             if(this.xpos+x<xlength&&this.ypos-Y<ylength&&this.xpos+x>=0&&(this.ypos-Y)>=0){range.add(new int[]{this.xpos+x, this.ypos-Y });}
             x+=1;
-        }*/
+        }
         //oben links
         Y=3;
         x=0;
